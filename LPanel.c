dbdb0e3de1c42d621c1c72f8acf3bff87d1b1ac0
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2020-10-19 17:23:55 -0500

    Update LPanel.c
    Thanks to marker5a for helping me squash out some bugs and guiding me! 
    
    Fixed the issue where inputting a character other than the numbers would cause it to crash, and then to loop...

--- LPanel.c
+++ LPanel.c
@@ -5,9 +5,11 @@ int main()
 
 {
 	system("clear");
-	int response = 0;
+	char response;
 	
 	do {
+		response=0;
+		system("clear");
 		printf("Hello! What would you like to do today?\n");
 	
 		printf("1) Update the system\n");
@@ -16,30 +18,17 @@ int main()
 		printf("4) Exit\n\n");
 		printf("Please enter your response: ");
 		
-		response = 0;
-		scanf("%d", &response);
-		system("clear");
+		response = getchar();
+		printf("\n");		
 
-
-			if (response <= 4){
-	
-				switch(response){
-        
-        				case 1 :
-            					system("/home/a/Documents/Update.sh");
-            				break;
-        				case 2 :
-            					system("/home/a/Documents/Wine_and_Lutris_script.sh");
-            				break;
-        				case 3 :
-            					system("/home/a/Documents/Software_Install.sh");
-            				break;
-           				case 4: 
-           				return 0;
-    					}
-				}
+		switch(response){       				
+			case '1' :	system("/home/a/Documents/Update.sh"); 			break;
+			case '2' :	system("/home/a/Documents/Wine_and_Lutris_script.sh"); 	break;
+			case '3' :	system("/home/a/Documents/Software_Install.sh");		break;
+			case '4' : 	return 0;							break;					
+    	}
 	}
-	while(response >= 5);
+	while(1);
 
 return 0;
 }
